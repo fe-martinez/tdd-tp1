fdca6b25c34078d1be92ab4c947339c23f26f9d1
Clara Ruano Frugoli <cruano@fi.uba.ar>
2024-04-14 21:37:41 -0300

    agregamos tabla de followers

--- src/database/databaseManager.ts
+++ src/database/databaseManager.ts
@@ -23,6 +23,7 @@ export class UserSQLiteManager {
     private createTables() {
         const createHobbiesTableQuery = userQueries.createHobbiesTableQuery;
         const createUserHobbiesTableQuery = userQueries.createUserHobbiesTableQuery;
+        const createUserFollowsTableQuery = userQueries.createUserFollowsTableQuery;
 
         const createUserTableQuery = `
             CREATE TABLE IF NOT EXISTS users (
@@ -39,7 +40,8 @@ export class UserSQLiteManager {
         this.db.run(createUserTableQuery);
         this.db.run(createHobbiesTableQuery);
         this.db.run(createUserHobbiesTableQuery);
-
+        this.db.run(createUserFollowsTableQuery);
+        
         const insertHobbiesQuery = `
             INSERT OR IGNORE INTO hobbies (name) VALUES (?)
         `;

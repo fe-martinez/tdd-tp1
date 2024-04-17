879235ce8c3078074d84c47e74ccff722be3f20c
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-04-16 23:16:46 -0300

    acomodo uso de jwt

--- src/routes/profile/index.ts
+++ src/routes/profile/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import { authenticateToken } from '../../middleware/jwt';
+import jwt from '../../middleware/jwt';
 import multer from 'multer';
 import controller from '../../controllers/profile';
 
 const router = express.Router();
 const upload = multer();
 
-router.post('/photo', upload.single('photo'), authenticateToken, controller.updatePhoto);
-router.get('/photo', authenticateToken, controller.getProfilePhoto);
-router.delete('/photo', authenticateToken, controller.deletePhoto);
+router.post('/photo', upload.single('photo'), jwt.authenticateToken, controller.updatePhoto);
+router.get('/photo', jwt.authenticateToken, controller.getProfilePhoto);
+router.delete('/photo', jwt.authenticateToken, controller.deletePhoto);
 
 export default router;

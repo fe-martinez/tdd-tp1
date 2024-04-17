296a33717701f3f77c1c5cd517170f3562429586
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-04-16 23:18:00 -0300

    refactor de funciones

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,39 +1,38 @@
 import HTTPErrorCodes from "../utilities/httpErrorCodes";
 import { Request, Response } from 'express';
-import { generateTokens } from '../middleware/jwt'
+import jwt from '../middleware/jwt'
 import bcrypt from 'bcrypt';
 import { UserService } from '../services/userService';
 
-const saltRounds = 10;
+function parseAuthorizationHeader(authHeader: string | undefined) {
+    const base64String = authHeader && authHeader.split(' ')[1];
+    if (!base64String) return {};
+    const [username, password] = Buffer.from(base64String, 'base64').toString().split(':');
+    return { username, password };
+}
 
-async function login(req: Request, res: Response) {
+function login(req: Request, res: Response) {
     const authHeader = req.headers['authorization'];
-    const basicAuthString = authHeader && authHeader.split(' ')[1];
-
-    if (!basicAuthString) {
-        return res.sendStatus(HTTPErrorCodes.Unauthorized);
+    const { username, password } = parseAuthorizationHeader(authHeader);
+    if (!username || !password) {
+        return res.sendStatus(HTTPErrorCodes.Forbidden);
     }
 
-    // Decodificamos el string de autorización
-    const [username, password] = Buffer.from(basicAuthString, 'base64').toString().split(':');
-
-    // Validamos contra la DB
     new UserService()
         .getUserByEmail(username)
         .then(user => {
             if (!user)
-                return res.status(HTTPErrorCodes.Unauthorized).json({ error: "User not found" });
+                return res.status(HTTPErrorCodes.NotFound).json({ error: "User not found" });
 
             const userPassword = user.password.toString();
             return bcrypt.compare(password, userPassword)
-                .then(equal => {
-                    if (!equal)
-                        return res.status(HTTPErrorCodes.Unauthorized).json({ error: "Password is not correct" });
+            .then(equal => {
+                if (!equal)
+                    return res.status(HTTPErrorCodes.Unauthorized).json({ error: "Password is not correct" });
 
-                    const email = user.email.toString();
-                    const tokens = generateTokens(user.id, email);
-                    return res.json(tokens);
-                })
+                const tokens = jwt.generateTokens(user.id, user.email.toString());
+                return res.json(tokens);
+            })
         })
         .catch(err => {
             const errMessage: string = err.message || "Database error";

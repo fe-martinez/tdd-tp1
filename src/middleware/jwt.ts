444161f7ccae78d4c96dc730ed79874ad89fb421
Ramiro Gestoso <rgestoso@fi.uba.ar>
2024-04-16 23:17:31 -0300

    uso authorization header para recibir refresh token en vez de body

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -6,16 +6,21 @@ const secretKey = 'tu_clave_secreta'; // Reemplaza esto con tu clave secreta
 const refreshTokens: string[] = []; // Almacena los refresh tokens válidos
 const accessTokenExpirationTime = 300; // 5 minutos
 
-// Función para generar un token de acceso
 function generateAccessToken(id: number, email: string): string {
     return jwt.sign({ id, email }, secretKey, { expiresIn: accessTokenExpirationTime });
 }
 
-// Función para generar un refresh token
 function generateRefreshToken(id: number, email: string): string {
     const refreshToken = jwt.sign({ id, email }, secretKey); // No especificamos tiempo de expiración
     refreshTokens.push(refreshToken); // Almacenamos el refresh token válido
     return refreshToken;
+}
+
+function consumeRefreshToken(token: string) {
+    const index = refreshTokens.indexOf(token);
+    if (index !== -1) {
+        refreshTokens.splice(index, 1);
+    }
 }
 
 function generateTokens(id: number, email: string) {
@@ -26,7 +31,7 @@ function generateTokens(id: number, email: string) {
 
 function authenticateToken(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = parseAuthorizationHeader(authHeader);
 
     if (!token || refreshTokens.includes(token)) {
         return res.sendStatus(HTTPErrorCodes.Unauthorized);
@@ -47,7 +52,8 @@ function authenticateToken(req: Request, res: Response, next: NextFunction) {
 }
 
 function refreshToken(req: Request, res: Response) {
-    const token: string | undefined = req.body?.refreshToken;
+    const authHeader = req.headers['authorization'];
+    const token = parseAuthorizationHeader(authHeader);
 
     if (!token || !refreshTokens.includes(token)) {
         return res.sendStatus(HTTPErrorCodes.Unauthorized);
@@ -58,14 +64,14 @@ function refreshToken(req: Request, res: Response) {
             return res.sendStatus(HTTPErrorCodes.Forbidden);
         }
 
-        const index = refreshTokens.indexOf(token);
-        if (index !== -1) {
-            refreshTokens.splice(index, 1);
-        }
+        consumeRefreshToken(token);
 
         res.json(generateTokens(user.id, user.email));
     });
 }
 
+function parseAuthorizationHeader(authHeader: string | undefined) {
+    return authHeader && authHeader.split(' ')[1];
+}
 
-export { generateTokens, authenticateToken, refreshToken };
+export default { generateTokens, authenticateToken, refreshToken };

3533655a22e7a6b8efae041becd2b32c5f56f08d
Paula Bruck <85425148+paulabruck@users.noreply.github.com>
2024-04-20 17:35:31 -0300

    Fecha actual o menor y contrasena con 1 mayuscula

--- src/model/userValidSchema.ts
+++ src/model/userValidSchema.ts
@@ -1,19 +1,20 @@
 import joi from 'joi';
 import { Gender } from './gender';
 
-const MIN_PASSWORD_LENGTH = 5;
+const MIN_PASSWORD_LENGTH = 8;
 const MIN_LAST_NAME_LENGTH = 2;
 const MAX_FIRST_NAME_LENGTH = 64;
+const UPPERCASEREGEX = /[A-Z]/;
 
 const userValidSchema = joi.object({
     firstName: joi.string().max(MAX_FIRST_NAME_LENGTH).required(),
     lastName: joi.string().min(MIN_LAST_NAME_LENGTH).required(),
     email: joi.string().email().required(),
-    password: joi.string().min(MIN_PASSWORD_LENGTH).required(),
+    password: joi.string().min(MIN_PASSWORD_LENGTH).regex(UPPERCASEREGEX).required(),
     photo: joi.string(),
-    birthDate: joi.date().required(),
+    birthDate: joi.date().max('now').required(),
     gender: joi.string().valid(...Object.values(Gender)).required(),
-    hobbies: joi.array().items(joi.string())
+    hobbies: joi.array().items(joi.number()).required()
 });
 
 export { userValidSchema };
